42be2411d58c04d77a0cee0753c33ce052f14a9a
MoazElzeny <moaz.elzeny@orial-eg.com>
2023-11-08 20:11:46 +0200

    adminHome

--- GoldenAirport.WebAPI/Controllers/Admin/HomeController.cs
+++ GoldenAirport.WebAPI/Controllers/Admin/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GoldenAirport.Application.AdminDetails.Queries;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoldenAirport.WebAPI.Controllers.Admin
 {
     [ApiController]
     [Route("api/v{version:apiVersion}/[controller]")]
-    public class HomeController : ControllerBase
+    public class HomeController : BaseController<HomeController>
     {
+        public HomeController() { }
+
+        public async Task<IActionResult> GetAdminDetails()
+        {
+
+            var query = new AdminHomeQuery { UserId = CurrentUserId };
+            var result = await Mediator.Send(query);
+            return !result.IsSuccess ? BadRequest(result.Error) : Ok(result.Result);
+        }
     }
 }
