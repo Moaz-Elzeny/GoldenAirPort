7b00e53125137d10192500c96a053248578da979
MoazElzeny <moaz.elzeny@orial-eg.com>
2024-02-25 19:05:10 +0200

    update

--- GoldenAirport.Application/Flights/Commands/GetFlightQuery.cs
+++ GoldenAirport.Application/Flights/Commands/GetFlightQuery.cs
@@ -36,12 +36,35 @@ namespace GoldenAirport.Application.Flights.Commands
 
 
 
-                ResponseThirdPartyDto<dynamic> responseDto = shoppingQuery.Default();
-                if (!responseDto.IsSuccess)
+                //ResponseThirdPartyDto<dynamic> responseDto = shoppingQuery.Default();
+
+                ResponseThirdPartyDto<dynamic> response = shoppingQuery.Default();
+
+                // Create a list of GetFlightDto
+                var flightDtos = new List<GetFlightDto>
+                {
+                    new GetFlightDto
+                    {
+                        FromCity = response.Result.groupedItineraryResponse?.version,
+                        //ToCity = response.Result.groupedItineraryResponse?.ToCity,
+                        //TakeOffDate = response.Result.groupedItineraryResponse?.TakeOffDate,
+                        //DateOfArrival = response.Result.groupedItineraryResponse?.DateOfArrival,
+                        //FlightHours = response.Result.groupedItineraryResponse?.FlightHours,
+                        //FlightPrice = response.Result.groupedItineraryResponse?.FlightPrice,
+                        //FlightPriceLessThanTwoYears = response.Result.groupedItineraryResponse?.FlightPriceLessThanTwoYears,
+                        //FlightPriceLessThanTwelveYears = response.Result.groupedItineraryResponse?.FlightPriceLessThanTwelveYears,
+                        //IsRefundable = response.Result.groupedItineraryResponse?.IsRefundable,
+                        //AirLine = response.Result.groupedItineraryResponse?.AirLine,
+                        //NumberOfSeats = response.Result.groupedItineraryResponse?.NumberOfSeats,
+                        //NumberOfBags = response.Result.groupedItineraryResponse?.NumberOfBags,
+                        //TravelClass = response.Result.groupedItineraryResponse?.TravelClass,
+                    }
+                };
+                if (!response.IsSuccess)
                     return ResponseDto<object>.Failure(new ErrorDto()
                     {
                         Message = "Error from Third Party",
-                        Detilse = responseDto.Result,
+                        Detilse = response.Result,
                     });
 
                 return ResponseDto<object>.Success(new ResultDto()
